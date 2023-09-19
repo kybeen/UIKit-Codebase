77788ba0039a566a4470fdab0f24c129af411354
kybeen <rei980627@gmail.com>
2023-09-20 03:05:23 +0900

    [Setting] 프로젝트 세팅 및 Root 뷰 컨트롤러 설정

---
+++ UIKitCodebase/UIKitCodebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitCodebase
+//
+//  Created by 김영빈 on 2023/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

3e3c092d30231ed4fc2511389e02f8fd9bf13c2d
kybeen <rei980627@gmail.com>
2023-09-20 12:58:15 +0900

    [Feat] 뷰에 Label 추가

--- UIKitCodebase/UIKitCodebase/ViewController.swift
+++ UIKitCodebase/UIKitCodebase/ViewController.swift
@@ -15,6 +15,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        setLabel()
+    }
+    
+    func setLabel() {
+        let rootLabel = UILabel()
+        /// view : 현재 뷰 컨트롤러가 관리하는 뷰
+        view.backgroundColor = .white
+        view.addSubview(rootLabel) // 뷰에 Label 추가
+        
+        rootLabel.text = "류트 뷰"
+        rootLabel.translatesAutoresizingMaskIntoConstraints = false
+        // 가운데 정렬
+        rootLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        rootLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
 
